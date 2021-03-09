8addff488cc8007a96b60c06158b31f13df4e0c5
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-03-09 14:18:57 -0700

    added getColorPalette to backend

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -3,8 +3,16 @@ class FilmsController < ApplicationController
     response = JSON.parse(RestClient.get("https://api.themoviedb.org/3/movie/upcoming?api_key=56f6662bff7c2a1c935c2575984e9f97&language=en-US&page=1"))
     render json: response
   end
+
   def show
     response = JSON.parse(RestClient.get("https://api.themoviedb.org/3/movie/#{params[:id]}?api_key=56f6662bff7c2a1c935c2575984e9f97&language=en-US"))
     render json: response
   end
+
+  def getColorPalette
+    Miro.options[:color_count] = params[:palette_num]
+    response = Miro::DominantColors.new("https://image.tmdb.org/t/p/w300#{params[:poster_path]}")
+    render json: response.to_hex
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :films, only: [:index, :show]
+  get '/getColorPalette', to: 'films#getColorPalette'
 end
