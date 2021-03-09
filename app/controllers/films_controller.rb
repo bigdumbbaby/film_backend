4613af77f6ea63311e2ff06ece9b476cc86c4f96
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-03-08 18:21:41 -0700

    first commit

---
+++ app/controllers/films_controller.rb
@@ -0,0 +1,6 @@
+class FilmsController < ApplicationController
+  def index 
+    response = JSON.parse(RestClient.get("https://api.themoviedb.org/3/movie/upcoming?api_key=56f6662bff7c2a1c935c2575984e9f97&language=en-US&page=1"))
+    render json: response
+  end
+end
