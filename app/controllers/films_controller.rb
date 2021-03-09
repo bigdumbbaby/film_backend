a67bdbe53f0a631eccf3a647bdd34f66b6cc94b4
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-03-09 11:29:58 -0700

    added show action to backend

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -3,4 +3,8 @@ class FilmsController < ApplicationController
     response = JSON.parse(RestClient.get("https://api.themoviedb.org/3/movie/upcoming?api_key=56f6662bff7c2a1c935c2575984e9f97&language=en-US&page=1"))
     render json: response
   end
+  def show
+    response = JSON.parse(RestClient.get("https://api.themoviedb.org/3/movie/#{params[:id]}?api_key=56f6662bff7c2a1c935c2575984e9f97&language=en-US"))
+    render json: response
+  end
 end
